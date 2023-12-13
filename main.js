ab662512d98441e944a7b4bb46048aa5bbfe7a02
Skunkler <42595638+Skunkler@users.noreply.github.com>
2023-12-12 19:27:28 -0500

    ndi test

--- main.js
+++ main.js
@@ -1,120 +1,97 @@
-function myFunction() {
-    const x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const emailValidation = (DomElem, e) =>
+{
+    console.log(DomElem);
+    let boolValue = e.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+    if (boolValue == null)
+    {
+        DomElem.className = "form-control is-invalid";
+    }
+    else if (boolValue != null)
+    {
+
+        DomElem.className = "form-control is-valid";
     }
 }
 
-const idListHeader = {
-    "#about": "About Stop Link 101", "#budget": "Budget Transparency",
-    "#testimonials": "Testimonials", "#signs": "Where and When To Get A Sign", "#upcoming":"Upcoming Events"
+
+const transition = async (prevElem) =>
+{
+    const idList = Object.keys(headerDict);
+    prevElem.className = "nav-item nav-link";
+    let prevTarget = prevElem.id.replace("Link", "");
+    let idx = idList.indexOf(prevTarget);
+    var prevDiv = document.getElementById(idList[idx]);
+    prevDiv.className = "hidden";
+
+    document.getElementById('#header').className = "text-center hidden";
+    await delay(1000);
+}
+
+const selectedLink = async (originatingElem, elementId) =>
+{
+    window.scrollTo(0,0);
+    const prevElem = document.getElementsByClassName("selectedLink")[0];
+    
+    if (prevElem != null) {
+        await transition(prevElem);
+    }
+    else
+    {
+        await transition(document.getElementById("#aboutLink"));
+    }
+    
+
+    originatingElem.className = "nav-item nav-link selectedLink";
+    getContent(elementId);
+}
+const headerDict = {
+    "#about": "About", "#services": "Services",
+    "#contact": "Contact"
 };
+const getContent = async (elementId) =>
+{
+    
+    let listIndexes = [];
+    let targetLink = 0;
 
-const videoSrc = "https://www.youtube.com/embed/dGcw17aAutQ?si=VEUTpwFYFcF48BEx";
+    const idList = Object.keys(headerDict);
 
-const showContent = (idName) => {
-    let idTargetIndex = 0;
-    let ListIndexes = [];
-    const idList = Object.keys(idListHeader);
-    idList.forEach(id => {
-        if (id === idName)
-        {
-            idTargetIndex = idList.indexOf(id);
+    idList.forEach(id =>
+    {
+        if (id === elementId) {
+            targetLink = idList.indexOf(id);
 
         }
-        else
-        {
-            ListIndexes.push(id);
+        else {
+            listIndexes.push(id);
         }
     });
-    const videoPlayer = document.querySelector("iframe");
 
-    if (idTargetIndex == 2)
-    {
-         videoPlayer.setAttribute('src', 'https://www.youtube.com/embed/dGcw17aAutQ?si=VEUTpwFYFcF48BEx');
-         document.getElementById(idList[idTargetIndex]).hidden = false;
-    }
-    else
-    {
-        videoPlayer.setAttribute('src', 'about:blank');
-        document.getElementById(idList[idTargetIndex]).hidden = false;
-    }
+    listIndexes.forEach(other => {
 
-    if (idList[idTargetIndex] == "#budget") {
+        let otherElem = document.getElementById(other);
+        otherElem.hidden = true;
 
-        removeBudget();
-        showBudget();
-    }
-    else
-    {
-        removeBudget();
-    }
+    })
 
-    document.getElementById('header').textContent = idListHeader[idList[idTargetIndex]];
-    ListIndexes.forEach(other => {
-        document.getElementById(other).hidden = true;
-    }) 
+
+    const element = document.getElementById(idList[targetLink]);
+    element.hidden = false;
+    element.className = "fade-in-text";
+    let header = document.getElementById('#header');
+    header.textContent = headerDict[idList[targetLink]];
+    header.className = "text-center fade-in-text";
+
+
 }
 
-const removeBudget = () =>
+
+const emailSubmission = (stuff) =>
 {
-    let table = document.getElementById("budgetTable");
-    if (table != null)
-    {
-        table.remove();
-    }
-}
-
-const showBudget = () => {
-
-
-    let table = document.createElement('table');
-    table.className = "table table-dark table-bordered border-primary";
-    table.id = "budgetTable"
-    let tableHead = document.createElement("thead");
-    let headerRow = document.createElement("tr");
-    let CostHeader = document.createElement("th");
-    let CostHeaderAmount = document.createElement("th");
-
-    let RevenueHeaderAmount = document.createElement("th");
-
-    CostHeader.scope = "col";
-    CostHeader.textContent = "Months";
-    CostHeaderAmount.textContent = "Cost Amount"
-    CostHeaderAmount.scope = "col"; 
-
-    RevenueHeaderAmount.scope = "col";
-    RevenueHeaderAmount.textContent = "Revenue Amount";
-
-    headerRow.appendChild(CostHeader);
-    headerRow.appendChild(CostHeaderAmount);
-    headerRow.appendChild(RevenueHeaderAmount);
-    tableHead.appendChild(headerRow);
-    table.appendChild(tableHead);
-    const monthList = ["September", "October", "November", "December"];
-    const costList = ["$200", "$500", "$1000", "$1150"];
-    const revenueList = ["$0", "$500", "$2200", "$2100"];
-
-
-    for (let i = 0; i < monthList.length; i++)
-    {
-
-        let tr = document.createElement('tr');
-
-        let costCategory = document.createElement('td');
-        let costCategoryAmount = document.createElement('td');
-        let revenueCategoryAmount = document.createElement('td');
-        costCategory.textContent = monthList[i];
-        costCategoryAmount.textContent = costList[i];
-        revenueCategoryAmount.textContent = revenueList[i];
-        tr.appendChild(costCategory);
-        tr.appendChild(costCategoryAmount);
-        tr.appendChild(revenueCategoryAmount);
-
-        table.appendChild(tr);
-    }
-    let targetElem = document.getElementById("#budget");
-    targetElem.appendChild(table)
+    console.log(stuff);
 }
